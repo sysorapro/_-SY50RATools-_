f78620d30d0007df6c1d420d8e618454f85c2765
sysorapro <sysora.pro@gmail.com>
2026-09-15 01:37:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,61 @@
-const CACHE_NAME = "sysora-tools-v1";
+const CACHE_NAME = "sysora-tools-v2";
 
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const URLS_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_SHELL);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
 
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
 
-  self.clients.claim();
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
 
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
+            return fetch(event.request).then(networkResponse => {
+                if (
+                    !networkResponse ||
+                    networkResponse.status !== 200 ||
+                    networkResponse.type !== "basic"
+                ) {
+                    return networkResponse;
+                }
 
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+                const responseToCache = networkResponse.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return networkResponse;
+            });
+        })
+    );
 });
